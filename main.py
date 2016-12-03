ab2ab32c3599fbe731e57495012ec036d65be05e
sneyens <neyens.s@gmail.com>
2016-12-03 09:54:20 -0800

    Can now view total questions in ingested exam via --count_questions; this does not allow testing.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from collections import Iterator
 
 # See example.json for example
 class Exam(Iterator):
-    def __init__(self, file_name= None, shuffle=False, shuffle_answers=False, limit=-1, test_mode=False):
+    def __init__(self, file_name= None, shuffle=False, shuffle_answers=False, limit=-1, test_mode=False, **kwargs):
         self.file_name = file_name
         self.exam = self.load_test()
         self.questions = self.exam['questions']
@@ -16,6 +16,9 @@ class Exam(Iterator):
         self.test_mode = test_mode
         self.correct = 0
         self.i = -1
+        self.count_questions = None
+        if kwargs:
+            self.count_questions = kwargs['count_questions']
 
     def load_test(self):
         exam_data = None
@@ -47,84 +50,89 @@ def main(exam):
     input = None
     current_question=0
     incorrect_questions=[]
-    for i, question in enumerate(exam):
-        current_question+=1
-        print "" # necessary for countdown??
-        print "Question(%s of %s): %s" % (current_question, exam.limit, question['question'])
-        answer_keys = question['answer_bank'].keys()
-        answers = map(str, question['answers'])
-        required_correct_answers = len(answers)
-        correct_answers = 0
-        #print required_correct_answers
+    if not exam.count_questions:
+        for i, question in enumerate(exam):
+            current_question+=1
+            print "" # necessary for countdown??
+            print "Question(%s of %s): %s" % (current_question, exam.limit, question['question'])
+            answer_keys = question['answer_bank'].keys()
+            answers = map(str, question['answers'])
+            required_correct_answers = len(answers)
+            correct_answers = 0
+            #print required_correct_answers
 
-        # Shuffle if enabled, else sort by key
-        if exam.shuffle_answers: random.shuffle(answer_keys)
-        else: answer_keys = sorted(answer_keys)
+            # Shuffle if enabled, else sort by key
+            if exam.shuffle_answers: random.shuffle(answer_keys)
+            else: answer_keys = sorted(answer_keys)
 
-        print ""
-        for k in answer_keys:
-            print k + ': ' + question['answer_bank'][k]
-
-        while True:
-            did_not_know_answer = False
             print ""
-            input = raw_input("Your answer [" + str(required_correct_answers) + "]: ").strip().upper()
-            if len(input):
+            for k in answer_keys:
+                print k + ': ' + question['answer_bank'][k]
 
-                if "P" in input:
-                    print "Passed."
-                    break
+            while True:
+                did_not_know_answer = False
+                print ""
+                input = raw_input("Your answer [" + str(required_correct_answers) + "]: ").strip().upper()
+                if len(input):
 
-                if "X" in input and not exam.test_mode:
-                    print answers
-                    print ""
-                    print "Explanation: %s" % question['explanation']
-                    break
-
-                if sorted(input) == sorted(answers):
-                    if exam.test_mode:
-                        exam.correct += 1
-                    else:
-                        print "Correct!"
-                        if len(question['explanation']):
-                            print "Explanation: %s" % question['explanation']
-                    break
-                else:
-                    if not exam.test_mode:
-                        print "Try again!"
-                    else:
-                        incorrect_questions.append(question)
+                    if "P" in input:
+                        print "Passed."
                         break
 
+                    if "X" in input and not exam.test_mode:
+                        print answers
+                        print ""
+                        print "Explanation: %s" % question['explanation']
+                        break
 
-            else:
-                print "An answer must be provided."
-                continue
+                    # Sort then uniquefy the input
+                    if sorted(input) == sorted(answers):
+                        if exam.test_mode:
+                            exam.correct += 1
+                        else:
+                            print "Correct!"
+                            if len(question['explanation']):
+                                print "Explanation: %s" % question['explanation']
+                        break
+                    else:
+                        if not exam.test_mode:
+                            print "Try again!"
+                        else:
+                            incorrect_questions.append(question)
+                            break
 
-        print ""
-        print raw_input("Next?")
+
+                else:
+                    print "An answer must be provided."
+                    continue
+
+            print ""
+            print raw_input("Next?")
+            os.system('clear')
+
+        if exam.test_mode:
+            print "Test score: %s / %s" % (exam.correct, exam.limit)
+            while True:
+                show_incorrect = raw_input("Do you wish to view the questions that were answered incorrectly (y/n)? ").lower()
+                if show_incorrect == "n": break
+                elif show_incorrect == "y":
+                    for j, inc_question in enumerate(incorrect_questions):
+                        print "Question %s: %s" % (j, inc_question['question'])
+                        print ""
+                        answer_keys = inc_question['answer_bank'].keys()
+                        for k in sorted(answer_keys):
+                            print k + ': ' + inc_question['answer_bank'][k]
+
+                        print ""
+                        print "Answer(s): %s" % (map(str,inc_question['answers']))
+                        print ""
+                        print "Explanation: %s" % inc_question['explanation']
+                        raw_input("Next?")
+                        os.system('clear')
+                break
+    else:
         os.system('clear')
-
-    if exam.test_mode:
-        print "Test score: %s / %s" % (exam.correct, exam.limit)
-        while True:
-            show_incorrect = raw_input("Do you wish to view the questions that were answered incorrectly (y/n)? ").lower()
-            if show_incorrect == "n": break
-            elif show_incorrect == "y":
-                for j, inc_question in enumerate(incorrect_questions):
-                    print "Question %s: %s" % (j, inc_question['question'])
-                    print ""
-                    answer_keys = inc_question['answer_bank'].keys()
-                    for k in sorted(answer_keys):
-                        print k + ': ' + inc_question['answer_bank'][k]
-
-                    print ""
-                    print "Answer(s): %s" % (map(str,inc_question['answers']))
-                    print ""
-                    print "Explanation: %s" % inc_question['explanation']
-                    raw_input("Next?")
-                    os.system('clear')
-            break
+        print "Total questions in exam: %s" % len(exam.questions)
 
 
 if __name__ == '__main__':
@@ -134,11 +142,12 @@ if __name__ == '__main__':
     parser.add_argument('-s', '--shuffle_answers', default=False, action='store_true')
     parser.add_argument('-t', '--test_mode', default=False, action='store_true')
     parser.add_argument('-l', '--question_limit', type=int, default=-1)
+    parser.add_argument('--count_questions', default=False, action='store_true')
     pargs = parser.parse_args()
 
     if pargs.file:
         os.system('clear')
-        exam=Exam(file_name=pargs.file, shuffle=pargs.random_questions, shuffle_answers=pargs.shuffle_answers, limit=pargs.question_limit, test_mode=pargs.test_mode)
+        exam=Exam(file_name=pargs.file, shuffle=pargs.random_questions, shuffle_answers=pargs.shuffle_answers, limit=pargs.question_limit, test_mode=pargs.test_mode, count_questions=pargs.count_questions)
         #t1=Thread(target=countdown)
         t2=Thread(target=main, args=(exam,))
         #t1.start()
